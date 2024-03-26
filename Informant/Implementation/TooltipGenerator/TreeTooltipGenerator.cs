35647fe6a7987bda353491a931f66a6b8f75ce49
DK <35783662+gottyduke@users.noreply.github.com>
2024-03-26 18:38:56 -0500

    feat: fixed mossy wild tree (#7)

--- Informant/Implementation/TooltipGenerator/TreeTooltipGenerator.cs
+++ Informant/Implementation/TooltipGenerator/TreeTooltipGenerator.cs
@@ -37,12 +37,18 @@ internal class TreeTooltipGenerator : ITooltipGenerator<TerrainFeature>
             case Tree.mushroomTree:
             case Tree.mahoganyTree:
             case Tree.palmTree2:
+            case Tree.mysticTree:
+                var treeString = _modHelper.Translation.Get("TreeTooltipGenerator.Type" + tree.treeType.Value);
+                if (tree.hasMoss.Value) {
+                    treeString = _modHelper.Translation.Get("TreeTooltipGenerator.MossCovered", new { X = treeString });
+                }
+                return treeString;
             case Tree.greenRainTreeBushy:
             case Tree.greenRainTreeLeafy:
             case Tree.greenRainTreeFern:
-            case Tree.mysticTree:
-                return _modHelper.Translation.Get("TreeTooltipGenerator.Type" + tree.treeType.Value);
+                return _modHelper.Translation.Get("TreeTooltipGenerator.Type10");
+            default:
+                return "???";
         }
-        return "???";
     }
 }
